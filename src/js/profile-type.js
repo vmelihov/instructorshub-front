48c9cb3bdc1ce302a89e5b9e6d20baf3806d7c2e
maksim.iakovlev <miakovlev@bellintegrator.com>
2019-05-07 11:39:30 +0300

    Выбор нескольких категорий при регистрации

--- src/js/profile-type.js
+++ src/js/profile-type.js
@@ -1,6 +1,18 @@
 jQuery(function ($) {
-    $('.js-select2').select2({
-        width: "100%"
+    select2Events = function () {
+        $('.js-categoryFaculty .js-select2').select2({
+            width: "100%"
+        });
+        $('.js-categoryFaculty .js-select2').on('select2:select', function (e) {
+            $(this).closest(".js-cfBlock").find(".d-none").removeClass("d-none");
+        });
+    }
+
+    select2Events();
+
+    $(".js-cfAddBlock").on("click", function () {
+        $(".js-categoryFaculty").append($(".js-cfBlockParent").html());
+        select2Events();
     });
 
     $(".js-showLink").on("click", function () {
